281aa8aaeecfab216272fb099814e974529b8500
Shubham <shubham2002bharat@gmail.com>
2021-06-27 22:19:06 +0530

    code written again

--- app.js
+++ app.js
@@ -2,19 +2,21 @@ var btnTranslate = document.querySelector("#btn-translate");
 var  txtInput= document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output-txt");
 var serverURL= "https://api.funtranslations.com/translate/minion.json";
-function getTranslatedURL(text) {
-    return serverURL + "?" + "text:" + text
+function getTranslatedURL(text){
+    return serverURL + "?" + "text=" + text
+}
+function errorHandler(error){
+    console.log("error",error)
+    alert("Ooops! something went wrong")
 }
 function clickHandler(){
-    var inputText = txtInput.value;
-    fetch(getTranslatedURL(inputText))
-    .then(response=> response.json())
-    .then(json=> {
-        var translatedText= json.contents.translated;
-        outputDiv.innerText=translatedText;
-    })
-   
-    // console.log("clicked!");
-    // console.log("input",txtInput.value);
+var inputText= txtInput.value;    
+fetch(getTranslatedURL(inputText))
+.then(response => response.json())
+.then(json =>{
+    var translatedText=json.contents.translated;
+    outputDiv.innerText= translatedText;
+})
+.catch(errorHandler)
 }
 btnTranslate.addEventListener("click",clickHandler);
